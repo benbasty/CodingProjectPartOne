9a4ddd7100fcec7d52fcbe7a9380e7f97e662a7d
benbasty <benbasty@yahoo.fr>
2021-10-13 20:16:17 +0800

    First comment.

---
+++ src/com/corejava/Main.java
@@ -0,0 +1,9 @@
+package com.corejava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to my java projects where i will review all the java projects and exercices learned during my practice ");
+    }
+}
